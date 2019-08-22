618663a18f724a8f3a6773a59fcba4261f43251a
Derevyannykh Aleksei <aleksei.derevyannykh@gmail.com>
2019-08-22 13:40:47 +0300

    Реализовали мин для реализации тестов

--- src/structures/LinearList.java
+++ src/structures/LinearList.java
@@ -30,31 +30,60 @@ public class LinearList<T> implements IList<T> {
     public void add(T elementToAdd, int index) {
         if (index < 0 || index > sizeList)
             throw new IllegalArgumentException("Not correct index");
+
         if (sizeList == 0)
             items[start] = elementToAdd;
-        else if (sizeList < items.length) {
 
+        else if (sizeList < items.length) { // Резерв есть!
+            if (index < sizeList / 2) { // Вставляем в первую половну списка
+                if (start == 0) { // Нет резерва в начале массива
+                    // Добавить резерв слева, вставить элемент слева и весь список передвинуть вправо
+                }
+                else { // Есть резерв в начале массива
+                    // Вставляем элемент влево и все что левее смещаем влево
+                }
+            }
+            else { // Вставляем во вторую половину списка
+                if (start + sizeList < items.length) { // Есть резерв в конце массива
+                    // Вставляем элемент вправо и остаток списка двигаем вправо
+                    addRight(elementToAdd, index);
+                }
+                else { // Нет резерва в конце массива
+                    // Вставляем элемент слева, всё что левее его смещаем влево
+                }
+            }
         }
-        else {
-            int newStart = start;
-
-            if (newStart == 0 && index < sizeList / 2)
-                newStart = (items.length * 2 - sizeList) / 2;
-
-            Object[] newItems = new Object[items.length * 2];
-            for (int i = 0; i < index; i++)
-                newItems[newStart + i] = items[start + i];
-
-            newItems[newStart + index] = elementToAdd;
-
-            for (int i = index; i <sizeList ; i++)
-                newItems[newStart + i + 1] = items[start + i];
-
-            items = newItems;
-        }
+        else // Резерва нет!! пересоздаение массива!
+            recreateItems(elementToAdd, index);
 
         sizeList++;
     }
+
+    private void addRight (T elementToAdd, int index) {
+        for (int i = sizeList; i > index ; i--)
+            items[i] = items[i - 1];
+        items[index] = elementToAdd;
+    }
+
+    private void recreateItems(T elementToAdd, int index) {
+        int newStart = start;
+
+        if (newStart == 0 && index < sizeList / 2)
+            newStart = (items.length * 2 - sizeList) / 2;
+
+        Object[] newItems = new Object[items.length * 2];
+        for (int i = 0; i < index; i++)
+            newItems[newStart + i] = items[start + i];
+
+        newItems[newStart + index] = elementToAdd;
+
+        for (int i = index; i <sizeList ; i++)
+            newItems[newStart + i + 1] = items[start + i];
+
+        items = newItems;
+        start = newStart;
+    }
+
     @Override
     public void remove(int index) {
 
---
+++ test/structures/LinearListTest.java
@@ -0,0 +1,16 @@
+package structures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinearListTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+}
