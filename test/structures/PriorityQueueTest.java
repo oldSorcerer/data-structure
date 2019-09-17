5ac3d8520170b29bb4ce5a800c69d5195d9a739b
Derevyannykh Aleksei <aleksei.derevyannykh@gmail.com>
2019-09-18 00:23:19 +0300

    добавил лийний test

--- test/structures/PriorityQueueTest.java
+++ test/structures/PriorityQueueTest.java
@@ -39,4 +39,25 @@ class PriorityQueueTest {
         assertEquals(60, queue.get());
         assertEquals(85, queue.get());
     }
+
+    @Test
+    void getTwo() {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(true);
+        queue.put(85);
+        queue.put(59);
+        queue.put(12);
+        queue.put(9);
+        queue.put(20);
+        queue.put(31);
+        queue.put(60);
+
+        assertEquals(9, queue.get());
+        assertEquals(12, queue.get());
+        assertEquals(20, queue.get());
+        assertEquals(31, queue.get());
+        assertEquals(59, queue.get());
+        assertEquals(60, queue.get());
+        assertEquals(85, queue.get());
+    }
+
 }
