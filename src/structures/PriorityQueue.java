e6e258aada4eba83718177769c4dfde7d90627a1
Derevyannykh Aleksei <aleksei.derevyannykh@gmail.com>
2019-09-17 22:47:37 +0300

    обновлил очередь с приоритетом, создал тесты.

--- src/structures/PriorityQueue.java
+++ src/structures/PriorityQueue.java
@@ -2,23 +2,80 @@ package structures;
 
 public class PriorityQueue <T extends Comparable <T>> implements IQueue<T> {
 
+    private boolean min;
+    private LinearList <T> list = new LinearList<>();
+
+    public PriorityQueue(boolean min) {
+        this.min = min;
+    }
+
+    private int leftChild(int number){
+        number = 2 * number + 1;
+        if (number >= list.getSizeList())
+            return -1;
+        return number;
+    }
+
+    private int rightChild(int number) {
+        number = 2 * number + 2;
+        if (number >= list.getSizeList())
+            return -1;
+        return number;
+    }
+
+    private int parent(int number) {
+        if (number == 0)
+            return -1;
+        return (number - 1) / 2;
+    }
+
     @Override
     public int getSizeQueue() {
-        return 0;
+        return list.getSizeList();
     }
 
     @Override
     public void put(T elementToAdd) {
+        list.add(elementToAdd);
+        int idx = list.getSizeList() - 1;
 
+        while (parent(idx) >= 0 && Utils.compare(list.get(idx), list.get(parent(idx)), min)) {
+            list.set(list.get(parent(idx)), idx);
+            list.set(elementToAdd, parent(idx));
+            idx = parent(idx);
+        }
     }
 
     @Override
     public T firstElement() {
-        return null;
+        return list.get(0);
     }
 
     @Override
     public T get() {
-        return null;
+        T element = list.get(0);
+        list.set(list.get(list.getSizeList() - 1), 0);
+        list.remove(list.getSizeList() - 1);
+        int idx = 0;
+
+        while ( (leftChild(idx) >= 0 && Utils.compare(list.get(leftChild(idx)), list.get(idx), min) )  ||
+                (rightChild(idx) >= 0 && Utils.compare(list.get(rightChild(idx)), list.get(idx), min) )   ) {
+            // Меняем на правого ребёнка, когда есть правый ребёнок, который приоритетнее, чем левый
+            // иначе меняем на левого
+            if (rightChild(idx) >= 0 && Utils.compare( list.get(rightChild(idx)), list.get(leftChild(idx)), min)) {
+                T tmp = list.get(idx);
+                list.set(list.get(rightChild(idx)), idx);
+                list.set(tmp, rightChild(idx));
+                idx = rightChild(idx);
+            }
+            else {
+                T tmp = list.get(idx);
+                list.set(list.get(leftChild(idx)), idx);
+                list.set(tmp, leftChild(idx));
+                idx = leftChild(idx);
+            }
+        }
+
+        return element;
     }
 }
---
+++ test/structures/PriorityQueueTest.java
@@ -0,0 +1,43 @@
+package structures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriorityQueueTest {
+
+    @Test
+    void put() {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(true);
+        queue.put(85);
+        queue.put(59);
+        queue.put(12);
+        queue.put(9);
+        queue.put(20);
+        queue.put(31);
+        queue.put(60);
+
+        assertEquals(9, queue.firstElement());
+    }
+
+    @Test
+    void get() {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(true);
+        queue.put(85);
+        queue.put(59);
+        queue.put(12);
+        queue.put(9);
+        queue.put(20);
+        queue.put(31);
+        queue.put(60);
+
+        assertEquals(9, queue.get());
+        assertEquals(12, queue.get());
+        assertEquals(20, queue.get());
+        assertEquals(31, queue.get());
+        assertEquals(59, queue.get());
+        assertEquals(60, queue.get());
+        assertEquals(85, queue.get());
+
+    }
+}
