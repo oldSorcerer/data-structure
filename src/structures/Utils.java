928df9f6892a899b205c90b07e1566d09510e6b6
svyatogorovai <andreysvyatoy@gmail.com>
2019-11-14 12:40:04 +0300

    add comments for Utils

--- src/structures/Utils.java
+++ src/structures/Utils.java
@@ -2,12 +2,16 @@ package structures;
 
 class Utils {
 
+    //TODO: Что будет, если T будет не Comparable?
     public static <T> boolean compare(T element1, T element2, boolean back) {
         if (back)
             return element1 == null || ((Comparable<T>) element1).compareTo(element2) < 0;
         else
             return element1 != null && ((Comparable<T>) element1).compareTo(element2) > 0;
     }
+
+    //TODO: Зачем тебе вообще поиск екстремума, если в списках он не нужен, это математическая операция жестко завязанная
+    //      сравнивать объекты у тебя лист может содержать элементы не Компарабельные, что тогда твой екстремум будет возвращать?
 
     public static <T> T extremum (IList<T> list, boolean max) {
 
