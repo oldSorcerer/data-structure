892a98aa3e98958d9afee9e4d79596f4543bebdb
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-03 20:48:15 +0300

    fix test method QueueTest

--- src/test/java/structures/List/QueueTest.java
+++ src/test/java/structures/List/QueueTest.java
@@ -1,71 +1,71 @@
 package structures.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
+    private Queue<String> stringQueue;
 
-    @Test
-    void put() {
-        Queue<String> stringQueue = new Queue<>();
+    @BeforeEach
+    public void init() {
+        stringQueue = new Queue<>();
+    }
+
+    public void fillQueue() {
         stringQueue.put("Привет");
         stringQueue.put("меня");
         stringQueue.put("зовут");
         stringQueue.put("Собака");
+    }
+
+    @Test
+    void put() {
+        fillQueue();
         assertEquals(4, stringQueue.getSizeQueue());
         assertEquals("Привет", stringQueue.get());
         assertEquals("меня", stringQueue.get());
-        assertEquals(2, stringQueue.getSizeQueue());   }
+        assertEquals(2, stringQueue.getSizeQueue());
+    }
 
     @Test
     void get() {
-        Queue <String> stringQueue = new Queue<>();
         assertNull(stringQueue.get());
         assertEquals(0, stringQueue.getSizeQueue());
     }
 
     @Test
     void firstElement() {
-        Queue <String> stringStack = new Queue<>();
-        stringStack.put("Привет");
-        stringStack.put("меня");
-        stringStack.put("зовут");
-        stringStack.put("Собака");
-        assertEquals(4, stringStack.getSizeQueue());
-        assertEquals("Привет", stringStack.firstElement());
-        assertEquals("Привет", stringStack.firstElement());
-        assertEquals(4, stringStack.getSizeQueue());
-
+        fillQueue();
+        assertEquals(4, stringQueue.getSizeQueue());
+        assertEquals("Привет", stringQueue.firstElement());
+        assertEquals("Привет", stringQueue.firstElement());
+        assertEquals(4, stringQueue.getSizeQueue());
     }
 
     @Test
     void lastElement() {
-        Queue <String> stringStack = new Queue<>();
-        stringStack.put("Привет");
-        stringStack.put("меня");
-        stringStack.put("зовут");
-        stringStack.put("Собака");
-        assertEquals(4, stringStack.getSizeQueue());
-        assertEquals("Собака", stringStack.lastElement());
-        assertEquals("Собака", stringStack.lastElement());
-        assertEquals(4, stringStack.getSizeQueue());
+        fillQueue();
+        assertEquals(4, stringQueue.getSizeQueue());
+        assertEquals("Собака", stringQueue.lastElement());
+        assertEquals("Собака", stringQueue.lastElement());
+        assertEquals(4, stringQueue.getSizeQueue());
     }
 
     @Test
-    void putNull(){
+    void pushNullException(){
+        assertThrows(IllegalArgumentException.class, () -> stringQueue.put(null));
+    }
+
+    @Test
+    void putNullMessage(){
         try {
-            Queue<String> stringStack = new Queue<>();
-            stringStack.put(null);
+            stringQueue.put(null);
             fail("No exception");
         }
         catch (IllegalArgumentException e){
-            // assertEquals("New element cannot be null", e.getMessage());
+            assertEquals("New element cannot be null", e.getMessage());
         }
-
-
     }
-
-
-
 }
